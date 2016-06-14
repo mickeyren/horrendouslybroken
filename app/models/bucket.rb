cf854cb656ad804b089f90544510d8d29d708e2d
David Ang <davidang09@gmail.com>
2016-06-14 18:12:12 +0800

    required validations for the bucket

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,3 +1,7 @@
 class Bucket < ActiveRecord::Base
   has_many :posts
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 64 }
+
+  validates :description, presence: true, length: { maximum: 1500 }
 end
